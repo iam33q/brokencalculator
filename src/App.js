2ffbfcb9537668ceb9b3bd100c3a9002b31369d5
iam33q <irisandrei97@gmail.com>
2021-08-24 18:15:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import React from "react";
 import "./App.scss"; 
 
@@ -7,51 +7,46 @@ function App() {
   const [display, setDisplay] = useState(" ");
   const [operationDisplay, setOperationDisplay] = useState("");
   const [lastOperator, setLastOperator] = useState("");
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("Just use your keyboard");
   const [resultDisplay, setResultDisplay] = useState("");
-
+  // const [number1, number2, number3, number4, number5,number6, number7, number8, number9, number0] = [useRef(), useRef(), useRef(), useRef(), useRef(), useRef(), useRef(), useRef(), useRef(), useRef()]
+  
   useEffect(() => {
     const keypresses = (event) => {
-      console.log(event.key)
-      switch(event.key){
-        case "+":operatorClicked("+"); break;
-        case "-":operatorClicked("-"); break;
-        case "*":operatorClicked("*"); break;
-        case "/":operatorClicked("/"); break;
-        case "0":handleDisplayWithKey("0"); break;
-        case "1":handleDisplayWithKey("1"); break;
-        case "2":handleDisplayWithKey("2"); break;
-        case "3":handleDisplayWithKey("3"); break;
-        case "4":handleDisplayWithKey("4"); break;
-        case "5":handleDisplayWithKey("5"); break;
-        case "6":handleDisplayWithKey("6"); break;
-        case "7":handleDisplayWithKey("7"); break;
-        case "8":handleDisplayWithKey("8"); break;
-        case "9":handleDisplayWithKey("9"); break;
-      }
-    }
+        if (event.key == "0"||"1"||"2"||"3"||"4"){
+          setDisplayWithEvent(Number(event.key));
+          console.log("|",display,"|", lastOperator,"|", operationDisplay,"|");
+        } else if (event.key == "5"||"6"||"7"||"8"||"9"){
+          setDisplayWithEvent(Number(event.key));
+          console.log("|",display,"|", lastOperator,"|", operationDisplay,"|");
+        } else if (event.key == "c"){
+          Clear();
+        } 
+        switch(event.key){
+          case "+": operatorClicked(event.key)
+          case "-": operatorClicked(event.key)
+          case "*": operatorClicked(event.key)
+          case "/": operatorClicked(event.key)
+        }
 
-    document.addEventListener('keydown', keypresses);
+      }
+      
+  document.addEventListener('keydown', keypresses);
     return function cleanup() {
       document.removeEventListener('keydown', keypresses);
     }
   }, [])
 
-
   function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
   }
-  const handleDisplayWithKey = (value) =>{
-    if (display == 0){setDisplay(value)}
-    let newDisplay = new String(display + value);
-    setDisplay(newDisplay);
-  }
 
-  const setDisplayWithEvent = (event) => {
-    console.log(event)
-    if (display == 0){setDisplay(event.target.value)}
-    let newDisplay = display + event.target.value;
-    setDisplay(newDisplay);
+  const setDisplayWithEvent = (number) => {
+    if (number == null || undefined || ""){return false}
+    if (display === 0){setDisplay("")}
+    console.log(display, number)
+    setDisplay(display + number);
+    console.log(display, lastOperator, operationDisplay);
   };
   
   function Clear() {
@@ -67,6 +62,13 @@ function App() {
     setDisplay(ans);
   }
 
+  function operatorClicked(value){
+    let mitmdisplay = display;
+    setLastOperator(value);
+    setOperationDisplay(mitmdisplay);
+    setDisplay("");
+  }
+
   const equalsClicked = () => {
     if(lastOperator == !("+"||"-"||"*"||"/")){
       setMessage("Please select an operator.")
@@ -75,6 +77,7 @@ function App() {
       setMessage("Please add a number to compute.")
       return false;
     }
+
     let calc = 0;
     console.log(calc,display,lastOperator,operationDisplay)
       switch(lastOperator){
@@ -82,33 +85,43 @@ function App() {
         case "-": calc += (Number(operationDisplay) - Number(display)); break;
         case "*": calc += (Number(operationDisplay) * Number(display)); break;
         case "/": calc += (Number(operationDisplay) / Number(display)); break;
+        default : setMessage("Unknown Operator");
       }
+
     setResultDisplay(calc);
     setDisplay("");
     setOperationDisplay("");
     setLastOperator("");
     setMessage("");
     }
-
   return (
   <div className="App App-header">
     <div className="calculatordisplay">{operationDisplay}{lastOperator}{display}</div><br />
+    <div>
+      {/* <button value="0" key="0" onClick={setDisplayWithEvent(0)} >0</button><br/>
+      <button value="1" key="1" onClick={setDisplayWithEvent(1)} >1</button><br/>
+      <button value="2" key="2" onClick={setDisplayWithEvent(2)} >2</button><br/>
+      <button value="3" key="3" onClick={setDisplayWithEvent(3)} >3</button><br/>
+      <button value="4" key="4" onClick={setDisplayWithEvent(4)} >4</button><br/>
+      <button value="5" key="5" onClick={setDisplayWithEvent(5)} >5</button><br/>
+      <button value="6" key="6" onClick={setDisplayWithEvent(6)} >6</button><br/>
+      <button value="7" key="7" onClick={setDisplayWithEvent(7)} >7</button><br/>
+      <button value="8" key="8" onClick={setDisplayWithEvent(8)} >8</button><br/>
+      <button value="9" key="9" onClick={setDisplayWithEvent(9)} >9</button><br/> 
+      
+      some BS about to many re-renders*/}
 
-    <div>{[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(function (number,i){
-      return <button value={number} key={i} onClick={setDisplayWithEvent}>{number}</button>})}<br/>
     </div>
-
-    <button value="c" onClick={Clear}>C</button>
-    <button value="=" onClick={equalsClicked}>   =</button>
-    <button value="a" onClick={useResult}>Ans</button><br/>
-    
+{/* 
+    <button onClick={Clear}>C           </button>
+    <button onClick={equalsClicked}>   =</button> */}
+    {/* <button onClick={useResult}>Ans     </button><br/>
     <button id="counter1" value="+" onClick={() => operatorClicked("+")}>  +</button>
     <button id="counter2" value="-" onClick={() => operatorClicked("-")}>  -</button>
     <button id="counter3" value="*" onClick={() => operatorClicked("*")}>  *</button>
-    <button id="counter4" value="/" onClick={() => operatorClicked("/")}>  /</button>
-    <br/>
-    <span>{resultDisplay}</span><br/>
-    <span>{message}</span>
+    <button id="counter4" value="/" onClick={() => operatorClicked("/")}>  /</button> <br/> */}
+    <span>  {resultDisplay} </span> <br/>
+    <span>  {message}       </span>
   </div>
   );
 }
